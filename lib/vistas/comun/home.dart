c4a34800a7a79c7a0c44999d9c84cb4b1e09ee62
Diego Martinez <martinezd@presidencia.gob.ec>
2025-01-24 15:23:43 -0500

    Creación de un formulario con validaciones

--- lib/vistas/comun/home.dart
+++ lib/vistas/comun/home.dart
@@ -100,11 +100,21 @@ class RecipeForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // * variable global...
-    final _formKey = GlobalKey<FormState>();
+    // * servirá como validador...
+    final formKey = GlobalKey<FormState>();
+    // * variable de control de formulario que guarda su estado...
+    final TextEditingController textEditingControllerNombre =
+        TextEditingController();
+    final TextEditingController textEditingControllerAutor =
+        TextEditingController();
+    final TextEditingController textEditingControllerUrl =
+        TextEditingController();
+    final TextEditingController textEditingControllerReceta =
+        TextEditingController();
     return Padding(
         padding: EdgeInsets.all(8),
         child: Form(
-          key: _formKey,
+          key: formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment
                 .start, // * alineación verifical - iniciando desde el principio
@@ -119,28 +129,95 @@ class RecipeForm extends StatelessWidget {
               SizedBox(
                 height: 16,
               ),
-              _buildTextField(label: 'Recipe Name'),
+              _buildTextField(
+                  textEditingController: textEditingControllerNombre,
+                  labelEtiqueta: 'Nombre',
+                  validator: (valor) {
+                    // * verifica si no existe datos...
+                    if (valor == null || valor.isEmpty) {
+                      return "Ingrese un nombre de la receta";
+                    }
+                    // * todo bien retorna null
+                    return null;
+                  }),
               SizedBox(
                 height: 16,
               ),
-              _buildTextField(label: 'Autor'),
+              _buildTextField(
+                  textEditingController: textEditingControllerAutor,
+                  labelEtiqueta: 'Autor',
+                  validator: (valor) {
+                    // * verifica si no existe datos...
+                    if (valor == null || valor.isEmpty) {
+                      return "Ingrese nombre de autor";
+                    }
+                    // * todo bien retorna null
+                    return null;
+                  }),
               SizedBox(
                 height: 16,
               ),
-              _buildTextField(label: 'Url imagen'),
+              _buildTextField(
+                  textEditingController: textEditingControllerUrl,
+                  labelEtiqueta: 'Url imagen',
+                  validator: (valor) {
+                    // * verifica si no existe datos...
+                    if (valor == null || valor.isEmpty) {
+                      return "Ingrese la url de la imagen";
+                    }
+                    // * todo bien retorna null
+                    return null;
+                  }),
               SizedBox(
                 height: 16,
               ),
-              _buildTextField(label: 'Receta')
+              _buildTextField(
+                  maxLines: 4,
+                  textEditingController: textEditingControllerReceta,
+                  labelEtiqueta: 'Receta',
+                  validator: (valor) {
+                    // * verifica si no existe datos...
+                    if (valor == null || valor.isEmpty) {
+                      return "Ingrese un nombre de la receta";
+                    }
+                    // * todo bien retorna null
+                    return null;
+                  }),
+              SizedBox(
+                height: 16,
+              ),
+              Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        //Navigator.pop(context);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    child: const Text(
+                      'Guardar',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    )),
+              )
             ],
           ),
         ));
   }
 
-  Widget _buildTextField({required String label}) {
+  Widget _buildTextField(
+      {required TextEditingController textEditingController,
+      required String labelEtiqueta,
+      required String? Function(String?) validator,
+      int maxLines = 1}) {
     return TextFormField(
       decoration: InputDecoration(
-          labelText: label,
+          labelText: labelEtiqueta,
           labelStyle: TextStyle(
             fontFamily: 'Quicksand',
             color: Colors.orange,
@@ -151,6 +228,8 @@ class RecipeForm extends StatelessWidget {
           focusedBorder: OutlineInputBorder(
               borderSide: BorderSide(color: Colors.orange, width: 1),
               borderRadius: BorderRadius.circular(10))),
+      validator: validator,
+      maxLines: maxLines,
     );
   }
 }
